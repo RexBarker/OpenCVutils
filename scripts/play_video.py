ded31ffa0517f9604b27df76bf9d6011ca5c24c6
RexBarker <rexbarker2017@gmail.com>
2020-05-05 15:03:22 +0200

    modified play_video to accept directories

--- scripts/play_video.py
+++ scripts/play_video.py
@@ -1,5 +1,7 @@
+import os
 import cv2
 import argparse
+from glob import glob
 from time import time, sleep
 
 parser = argparse.ArgumentParser()
@@ -10,46 +12,79 @@ parser.add_argument('--infile', type=str, required=True,
 parser.add_argument('--fps', type=int, default=60, 
                     help="video replay frame rate, frames per second")
 
+parser.add_argument('--rotate_right', action='store_true', 
+                    help="Rotate image by 90 deg clockwise")
+
+parser.add_argument('--rotate_left', action='store_true', 
+                    help="Rotate image by 90 deg anticlockwise")
+
 parser.add_argument('--info', action='store_true', 
                     help="output video information")
 
-args = parser.parse_args()
-mp4file = args.infile 
 
-fps = args.fps
-spf = float(1.0/fps)
+##### Helper functions #####
+def get_frame(vfile):
+    if os.path.isdir(vfile):
+        images = glob(os.path.join(vfile, '*.jp*'))
+        if not images:
+            images = glob(os.path.join(vfile, '*.png'))
+        assert images, f"No image file (*.jpg or *.png) found in {vfile}"        
 
-cap = cv2.VideoCapture(mp4file)
+        images = sorted(images,
+                        key=lambda x: int(x.split('/')[-1].split('.')[0]))
+        for img in images:
+            frame = cv2.imread(img)
+            yield frame
 
-n_frames = 0
-width,height = 0,0
-current = 0.0
-start = time()
+    else:
+        cap = cv2.VideoCapture(vfile)
+        while True:
+            ret, frame = cap.read()
+            if ret:
+                yield frame
+            else:
+                cap.release()
+                break
+                
+        
+if __name__ == '__main__': 
+    args = parser.parse_args()
 
-while True:
-    timediff = time() - current
+    vfile = args.infile
+    fps = args.fps
+    spf = float(1.0/fps)
 
-    if timediff < spf: 
-        sleep(spf - timediff)
+    n_frames = 0
+    width,height = 0,0
+    current = 0.0
+    start = time()
 
-    current = time()
+    for frame in get_frame(vfile):
+        timediff = time() - current
+
+        if timediff < spf: 
+            sleep(spf - timediff)
+
+        current = time()
          
-    ret, frame = cap.read()
-    if frame is None: break
+        height,width = frame.shape[:2]
+        n_frames += 1
 
-    height,width = frame.shape[:2]
-    n_frames += 1
+        if args.rotate_left:
+            frame = cv2.rotate(frame,cv2.ROTATE_90_COUNTERCLOCKWISE)
+        elif args.rotate_right:
+            frame = cv2.rotate(frame,cv2.ROTATE_90_CLOCKWISE)
 
-    cv2.imshow('frame',frame)
-    if cv2.waitKey(10) & 0xFF == ord('q'):
-         break
+        cv2.imshow('frame',frame)
+        if cv2.waitKey(10) & 0xFF == ord('q'):
+            break
 
-cap.release()
-cv2.destroyAllWindows()
+    #cap.release()
+    cv2.destroyAllWindows()
 
-actual_fps = n_frames / (time() - start)
+    actual_fps = n_frames / (time() - start)
 
-if args.info:
-    print(f"Number of frames: {n_frames}")
-    print(f"Width x height = ({width},{height})")
-    print(f"Actual replay speed = {actual_fps}/s")
+    if args.info:
+        print(f"Number of frames: {n_frames}")
+        print(f"Width x height = ({width},{height})")
+        print(f"Actual replay speed = {actual_fps}/s")
